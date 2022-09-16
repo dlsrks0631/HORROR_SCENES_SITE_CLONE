d38e483cdf665aa565f0753d5c5a3bd5a38868a4
dlsrks0631 <65211547+dlsrks0631@users.noreply.github.com>
2022-09-16 14:42:17 +0900

    horror_page_clone.create

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -11,7 +11,7 @@ const routes = {
   css: {
     watch: "src/scss/*",
     src: "src/scss/styles.scss",
-    dest: "dist/css",
+    dest: "dest/css",
   },
 };
 
@@ -32,7 +32,7 @@ const watch = () => {
   gulp.watch(routes.css.watch, styles);
 };
 
-const clean = async () => await deleteSync(["dist/"]);
+const clean = async () => await deleteSync(["dest/"]);
 
 const prepare = gulp.series([clean]);
 
